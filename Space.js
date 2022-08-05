ba2278854e8e3fa1b21f5c1db7114f62e097ac7b
DirtyHeroine <przemex17@wp.pl>
2022-08-05 14:34:48 +0200

    ByteConv Added

--- Space.js
+++ Space.js
@@ -19,4 +19,14 @@ function chkfunc(){
     });
 }
 
+// function ByteConv(number){
+//     var marker = 1024;
+//     var kiloBytes = marker; // One Kilobyte is 1024 bytes
+//     var megaBytes = marker * marker; // One MB is 1024 KB
+//     var gigaBytes = marker * marker * marker; // One GB is 1024 MB
+//     var gigaBytes = marker * marker * marker; // One GB is 1024 MB
+//     var teraBytes = marker * marker * marker * marker; // One TB is 1024 GB
+//     return result;
+// }
+
 module.exports = { chkfunc }
